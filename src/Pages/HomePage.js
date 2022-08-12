aa83b8987e4ed810adf36efd84f2f2189a5e3d2e
Kabir Hossain <104571104+kobir1989@users.noreply.github.com>
2022-08-12 16:45:08 +0600

    Footer added

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ApiRequest from '../Components/CountryList/ApiRequest';
-
+import './HomePage.css';
 const HomePage = () => {
   return (
     <div className='container__main'>
       <ApiRequest />
+      <div className='footer'>
+        <p>©Copyright 2022 | Kabir Hossain</p>
+      </div>
     </div>
   );
 };
