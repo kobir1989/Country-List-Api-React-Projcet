cc50c6eec22a380205ead322ee92308c49009a4a
Kabir Hossain <104571104+kobir1989@users.noreply.github.com>
2022-08-12 16:27:03 +0600

    useEffect hook missing dependency error fixed

--- src/Pages/CountryDetails.js
+++ src/Pages/CountryDetails.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-
 import { useParams } from 'react-router-dom';
 import './CountryDetails.css';
 import Details from './Details';
@@ -8,46 +7,47 @@ const CountryDetails = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const { id } = useParams();
-  const [countryData, setCountryData] = useState([]);
-  const getDataRequest = async () => {
-    setIsLoading(true);
-    setIsError(null);
-    try {
-      const response = await fetch(`https://restcountries.com/v3.1/name/${id}`);
-      if (!response.ok) {
-        throw new Error(isError);
-      }
-      const countryData = await response.json();
-
-      const recevedData = countryData.map((item) => {
-        return {
-          id: Math.floor(Math.random() * 1000),
-          flag: item.flags.png,
-          maps: item.maps.googleMaps,
-          name: item.name.common,
-          native: item.name.official,
-          coartOfArms: item.coatOfArms.png,
-          capital: item.capital,
-          region: item.region,
-          subRegion: item.subregion,
-          population: item.population,
-          area: item.area,
-          currenci: Object.keys(item.currencies),
-          Languae: Object.values(item.languages),
-          timezone: item.timezones,
-        };
-      });
-
-      setCountryData(recevedData);
-      setIsLoading(false);
-    } catch (error) {
-      setIsError(error);
-    }
-  };
+  const [singleCountryData, setSingleCountryData] = useState([]);
   useEffect(() => {
+    const getDataRequest = async () => {
+      setIsLoading(true);
+      setIsError(null);
+      try {
+        const response = await fetch(
+          `https://restcountries.com/v3.1/name/${id}`
+        );
+        if (!response.ok) {
+          throw new Error(isError);
+        }
+        const countryData = await response.json();
+
+        const recevedData = countryData.map((item) => {
+          return {
+            id: Math.floor(Math.random() * 1000),
+            flag: item.flags.png,
+            maps: item.maps.googleMaps,
+            name: item.name.common,
+            native: item.name.official,
+            coartOfArms: item.coatOfArms.png,
+            capital: item.capital,
+            region: item.region,
+            subRegion: item.subregion,
+            population: item.population,
+            area: item.area,
+            currenci: Object.keys(item.currencies),
+            Languae: Object.values(item.languages),
+            timezone: item.timezones,
+          };
+        });
+
+        setSingleCountryData(recevedData);
+        setIsLoading(false);
+      } catch (error) {
+        setIsError(error);
+      }
+    };
     getDataRequest();
-  }, []);
-  console.log(countryData);
+  }, [isError, id]);
   return (
     <div className='details__container'>
       {isLoading && !isError && <div className='spinar'></div>}
@@ -56,7 +56,7 @@ const CountryDetails = () => {
           <p>Status 404 Something went Wrong! Please try again.</p>
         </div>
       )}
-      <Details countryData={countryData} />
+      <Details countryData={singleCountryData} />
     </div>
   );
 };
