c82899c9e8eeb987bb169218346e9d84c2212055
Kabir Hossain <104571104+kobir1989@users.noreply.github.com>
2022-08-12 13:11:17 +0600

    Page Navigation button added and details page style modified

--- src/Components/CountryList/AllCountry.js
+++ src/Components/CountryList/AllCountry.js
@@ -7,12 +7,7 @@ const AllCountry = (props) => {
   return (
     <>
       <Card>
-        <div
-          className='country__card'
-          onClick={() => {
-            navigate(`/country-details/${props.name}`);
-          }}
-        >
+        <div className='country__card'>
           <img src={props.flag} alt='flag.png' />
           <h2> {props.name}</h2>
           <h3>Capital: {props.capital}</h3>
@@ -22,6 +17,14 @@ const AllCountry = (props) => {
             Area: {props.area}
             <sup>2</sup> Km
           </p>
+          <button
+            className='btn'
+            onClick={() => {
+              navigate(`/country-details/${props.name}`);
+            }}
+          >
+            Learn More
+          </button>
         </div>
       </Card>
     </>
